6feeaf5abe98b8096c896f1a0c4404796abd3be8
Delux1414 <drpg1008@gmail.com>
2023-11-21 11:44:23 -0500

    Add a star rating in mercadoLibre

--- mercadolibre/script.js
+++ mercadolibre/script.js
@@ -135,3 +135,36 @@ boxToggle.addEventListener("click", ()=> {
         buttonToggle.classList.add("button-toggle-off");
     }
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    var ratingInput = document.getElementById('ratingInput');
+    var calificarBtn = document.getElementById('calificarBtn');
+    var stars = document.querySelectorAll('.stars i');
+
+
+    calificarBtn.addEventListener('click', function () {
+
+        const ratingValue = parseFloat(ratingInput.value);
+        const fullStars = Math.floor(ratingValue);
+
+        
+
+        stars.forEach((star, index) => {
+            star.className = 'fa-regular fa-star';
+
+            if (index < fullStars) {
+                star.className = 'fa-solid fa-star';
+            } else if (index === fullStars) {
+                const remainder = ratingValue - fullStars;
+
+                if (remainder > 0.2 && remainder <= 0.7) {
+                    star.className = 'fa-solid fa-star-half-stroke';
+                } else if (remainder > 0.7) {
+                    star.className = 'fa-solid fa-star';
+                }
+            }
+        });
+    });
+});
